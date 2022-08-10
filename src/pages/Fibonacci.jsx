2c60bdd83d2148aa054886c4c96629eb9ee68e8b
Jowel Castaneda <jowell@kodego.ph>
2022-08-10 20:57:54 +0800

    First 3 exercises

--- src/pages/Fibonacci.jsx
+++ src/pages/Fibonacci.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+// Take input from the user
+const number = parseInt(prompt("Enter a positive number: "));
+
 function Fibonacci() {
-  return <div>Fibonacci</div>;
+  var first = 0;
+  var second = 1;
+  const sequence = [];
+
+  const generateFibonacci = () => {
+    // First 2 scenarios
+    if (number === 1) {
+      return <h2 className="text-primary">0</h2>;
+    } else if (number === 2) {
+      return [0, 1].map((data, index) => (
+        <h2 className="text-primary" key={index}>
+          {data}
+        </h2>
+      ));
+    }
+
+    // Third scenario
+    for (var i = 1; i <= number; i++) {
+      sequence.push(first);
+
+      var nextTerm = first + second;
+      first = second;
+      second = nextTerm;
+    }
+
+    return sequence.map((data, index) => (
+      <h2 className="text-primary" key={index}>
+        {data}
+      </h2>
+    ));
+  };
+
+  return (
+    <div className="p-5">
+      <h1>Enter the number of terms: {number}</h1>
+      <h2>Fibonacci Series: </h2>
+      {generateFibonacci()}
+    </div>
+  );
 }
 
 export default Fibonacci;
